3f53b6d4827ff1aef190e01cd6bf3f4373add19c
Güler kıray <kiraguler278@gmail.com>
2026-03-01 15:44:09 +0300

    supabase_client güncellendi

--- backend/app/core/supabase_client.py
+++ backend/app/core/supabase_client.py
@@ -1,5 +1,4 @@
+from supabase import create_client, Client
 from app.core.config import settings
-from supabase import create_client, Client, ClientOptions
 
-options = ClientOptions(headers={"x-supabase-role": "service_role"})
-supabase: Client = create_client(settings.SUPABASE_URL,settings.SUPABASE_SERVICE_KEY, options=options)
+supabase: Client = create_client(settings.SUPABASE_URL,settings.SUPABASE_SERVICE_KEY)
