507da9fc243f3ab93beca593fffb51a6df1b3fb5
Salim Caner <canersalim@outlook.com>
2026-03-05 13:16:53 +0300

    frontend kaldırıldı sadece backend kaldı

--- frontend/Home-frontend/js/pages/settings.js
+++
@@ -1,9 +0,0 @@
-/*Tema: Light/Dark (CSS class ile)
-kullanıcı bilgileri güncelleme */
-export function renderSettings() {
-  return `
-    <section class="page settings-page">
-      <h1>Ayarlar</h1>
-    </section>
-  `;
-}
